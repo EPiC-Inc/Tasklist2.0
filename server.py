f31e262a1c4118201a63d430fd61b57cdb5e2414
[R3DACT3D] <3picinc@gmail.com>
2020-04-03 17:43:03 -0600

    fuck

--- server.py
+++ server.py
@@ -1,3 +1,3 @@
 from app import app
 from os import environ
-app.run(port=int(os.environ.get('PORT', 5000)) host='0.0.0.0', debug=True)
+app.run(port=int(environ.get('PORT', 5000)), host='0.0.0.0', debug=True)
